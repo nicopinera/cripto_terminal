cb4ca47cdfb2dea68dc433eebcb23fa039cc5a8e
nicopinera <nicolaspinera@gmail.com>
2025-11-02 19:15:35 -0300

    Configuracion fail2ban

--- Makefile
+++ Makefile
@@ -5,3 +5,7 @@ config_ssh:
 config_pam:
 	chmod +x src/pam1.sh
 	sudo ./src/pam1.sh
+
+config_fail2ban:
+	chmod +x src/fail2ban.sh
+	sudo ./src/fail2ban.sh
--- src/fail2ban.sh
+++ src/fail2ban.sh
@@ -1,2 +1,19 @@
 set -o allexport
 source .env
+
+sudo apt update
+
+# instalo fail2ban e iptables
+sudo apt install fail2ban iptables -y
+
+# compruebo el estado del servicio fail2ban
+sudo systemctl status fail2ban.service
+
+cd /etc/fail2ban
+
+sudo cp jail.conf jail.local
+
+# edito el archivo de configuracion local
+sudo nano jail.local
+
+sudo systemctl restart fail2ban
