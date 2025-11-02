f033de9ce8bb2ff558ec10bdb85609a2b411fd13
nicopinera <nicolaspinera@gmail.com>
2025-11-02 19:08:47 -0300

    Correccion pam1.sh

--- src/pam1.sh
+++ src/pam1.sh
@@ -1,6 +1,14 @@
 set -o allexport
 source .env
 
+USER_NOT_PERMISSED="generico"
+
 LINE="auth required pam_listfile.so item=user sense=deny file=/etc/ssh/ssh.deny onerr=succeed"
 
 echo $LINE >> /etc/pam.d/sshd
+
+#creacion del fichero para negar acceso a usuarios especificos
+sudo touch /etc/ssh/ssh.deny
+
+# Agregado al final del usuario no permitido
+echo $USER_NOT_PERMISSED >> /etc/ssh/ssh.deny
